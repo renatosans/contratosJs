16075c0d9d46646e0ed64d5205c6529a24c508eb
renato_sans <renato@datacopy.com.br>
2022-07-25 18:22:58 -0300

    Ajustes

--- api/hello.js
+++ api/hello.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
+function sayHello(req, res) {
+    const dir = path.resolve('./public', 'img');
+    const filenames = fs.readdirSync(dir);
+    const images = filenames.map(name => path.join('/', 'img', name));
+    // res.json(images);
+
+    res.status(200).json({
+        message: Buffer.from(req.query.sampleText).toString('base64')
+    })
+}
 
 module.exports = {
-	default: (req, res) => {
-        const dir = path.resolve('./public', 'img');
-        const filenames = fs.readdirSync(dir);
-        const images = filenames.map(name => path.join('/', 'img', name));
-        // res.json(images);
-
-        res.status(200).json({
-            message: Buffer.from(req.query.sampleText).toString('base64')
-        })
-    }
+	default: (req, res) => sayHello(req, res),
+    get: (req, res) => sayHello(req, res),
 }
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,17 @@
 const { PrismaClient } = require('@prisma/client');
 
 
-const prisma = new PrismaClient()
+// dados para conexão com banco de dados serverless
+const host     = 'ghjomyw1mzfy.us-east-1.psdb.cloud';
+const username = Buffer.from('YzZiOHhxZG93bmt5', 'base64').toString('ascii');
+const password = Buffer.from('cHNjYWxlX3B3X0NPbUJZOVJQLVEzTGNzcnRCNmZLNHZjS3dGUk5NekJFUjhkUzN5SUJfWWs=', 'base64').toString('ascii');
+const port     = 3306
+const database = 'addoncontratos'
+const setSSL   = 'sslaccept=strict&sslmode=require'
+
+
+// DATABASE_URL="mysql://root:p@ssw0rd@localhost:3306/addoncontratos"
+const url = `mysql://${username}:${password}@${host}:${port}/${database}?${setSSL}`;
+const prisma = new PrismaClient({datasources: { db: { url: url } } })
 
 module.exports = prisma
