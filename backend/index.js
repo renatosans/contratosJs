7c533987c3cb0a1a60a3489e2a1233f69387c456
renato_sans <renato@datacopy.com.br>
2022-07-25 08:49:46 -0300

    Ajustes

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use("/", express.static('Public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(nextApi({ base: '/api', directory: 'api', options: {caseSensitive: false} }));
+app.use(nextApi({ base: '/api', directory: '../api', options: {caseSensitive: false} }));
 
 
 // inicia a API escutando na porta 3000
