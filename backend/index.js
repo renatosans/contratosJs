1518b3c0b2d9eeee49e819fce726cb47514f6849
renato_sans <renato@datacopy.com.br>
2022-08-04 09:05:17 -0300

    Ajustes

--- backend/index.js
+++ backend/index.js
@@ -14,19 +14,3 @@ app.use(nextApi({ base: '/api', directory: '../api', options: {caseSensitive: fa
 
 // inicia a API escutando na porta 3000
 app.listen(port, () => console.log('Express escutando chamadas na porta ' + port));
-
-/*
-// altera a senha
-app.post('/changePassword', (req, res) => {
-    const { username, oldPassword, newPassword, confirmation } = req.body;
-
-    const query = "SELECT * FROM login WHERE usuario='" + username + "' AND senha='" + oldPassword + "'";
-    mysqlConnection.query(query, (err, rows, fields) => {
-        if (!err) {
-            res.send(rows);
-        } else {
-            console.log("Error: " + JSON.stringify(err, undefined, 2));
-        }
-    })
-})
-*/
